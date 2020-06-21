bad4e7c7684971000fdfe51a24f374b12e5a282c
PurpleUltraViolet <purple.ultraviolet616@gmail.com>
2020-06-21 13:22:59 -0700

    Removed unnecessary nested loops

--- src/board.c
+++ src/board.c
@@ -7,58 +7,31 @@ int player = X_VAL;
 int opponent = O_VAL;
 
 int board_check(int *b) {
-    int winner = 0, drawpossible = 1, cstate, i, j, k, count;
+    static int winpos[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 0, 3, 6, 1, 4, 7, 2,
+        5, 8, 0, 4, 8, 2, 4, 6};
+    int winner = 0, drawpossible = 1, i;
 
     for(i = 0; i < 9; i++) {
         if(b[i] == 0) drawpossible = 0;
     }
 
     /* Check both players */
-    for(cstate = X_VAL, i = 0; i < 2; i++, cstate *= -1) {
-        /* Vertical check */
-        for(j = 0; j < 3; j++) {
-            count = 0;
-            for(k = 0; k < 3; k++) {
-                if(b[j + k * 3] == cstate) count++;
-            }
-            if(count == 3) {
-                winner = cstate;
-                goto END;
-            }
-        }
-
-        /* Horizontal check */
-        for(j = 0; j < 3; j++) {
-            count = 0;
-            for(k = 0; k < 3; k++) {
-                if(b[k + j * 3] == cstate) count++;
-            }
-            if(count == 3) {
-                winner = cstate;
-                goto END;
-            }
-        }
-
-        /* Diagonal check */
-        count = 0;
-        for(j = 0; j < 3; j++) {
-            if(b[j + j * 3] == cstate) count++;
-        }
-        if(count == 3) {
-            winner = cstate;
+    for(i = 0; i < 24; i += 3) {
+        if(b[winpos[i]] == X_VAL && b[winpos[i + 1]] == X_VAL &&
+                b[winpos[i + 2]] == X_VAL) {
+            winner = X_VAL;
             goto END;
         }
-        count = 0;
-        for(j = 0; j < 3; j++) {
-            if(b[(2 - j) + j * 3] == cstate) count++;
-        }
-        if(count == 3) {
-            winner = cstate;
+    }
+    for(i = 0; i < 24; i += 3) {
+        if(b[winpos[i]] == O_VAL && b[winpos[i + 1]] == O_VAL &&
+                b[winpos[i + 2]] == O_VAL) {
+            winner = O_VAL;
             goto END;
         }
     }
 
-    if(winner == 0 && !drawpossible) return 2;
+    if(!drawpossible) return 2;
 END:
     return winner;
 }
